8ff4af2735ebeeb56613e0329b6229b93fa1e3bc
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-22 12:41:56 +0600

    Dinamically usernames are shown in page

---
+++ js/api.js
@@ -0,0 +1,16 @@
+const loadData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json()).then(data => console.log(data));
+}
+document.getElementById('load-data').addEventListener('click', loadData);
+
+document.getElementById('load-user').addEventListener('click', () => {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data => displayUser(data));
+})
+const displayUser = (data) => {
+    const ul = document.getElementById('users');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = `Name: ${user.name}, Email: ${user.email}`;
+        ul.appendChild(li);
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+//JSON --> Javascript Object Notation
 const user = { id: 1, name: 'Amir', job: 'Actor' };
-console.log(user);
+const stringyfiedUser = JSON.stringify(user);
+
+
+const shop = {
+    name: 'Alia Store',
+    address: 'Ranbeer Road',
+    products: ['Laptop', 'Mobile', 'Pepsi'],
+    profit: 15000,
+    isExpensive: false,
+    owner: {
+        name: 'Alia Bhat',
+        profession: 'Actor'
+    }
+}
+const stringShop = JSON.stringify(shop);
+console.log(stringShop);
+const convertedShop = JSON.parse(stringShop);
+console.log(convertedShop.owner);
