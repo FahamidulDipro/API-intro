32df1a0d605bc15527c0c362e134bde96267e185
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-22 13:41:42 +0600

    function for adding post addPost added with POST method

--- js/post.js
+++ js/post.js
@@ -16,3 +16,20 @@ function showPosts(data) {
 }
 
 loadPosts();
+
+function addPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'My new Post',
+            body: 'This is my new Post',
+            userId: 100,
+
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+
+    }).then(res => res.json()).then(data => console.log(data));
+}
+addPost();
